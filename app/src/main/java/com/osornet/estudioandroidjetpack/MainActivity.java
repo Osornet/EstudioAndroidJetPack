410da5549dcbda3e444c250b2cdb09be6b78b32d
Manuel Osorio <manuelosorio@wposs.com>
2020-10-11 11:46:53 -0500

    Clase S05-CL02 Y CL03
    Se Agregan los elementos en el ConstraintLayout y se se guian con los
    Guidelines en donde se puede editar el el porsentaje de la posicion, con
    respecto a la pantalla.

--- app/src/main/java/com/osornet/estudioandroidjetpack/MainActivity.java
+++ app/src/main/java/com/osornet/estudioandroidjetpack/MainActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+
+/**
+ * Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
